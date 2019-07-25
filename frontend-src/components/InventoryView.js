c28c05fbb2d18f58f3199e65c6da098ab31c2742
Adrian Calderon <adrian@adriancalderon.com>
2019-07-25 07:36:37 -0400

    BBQ-20 Updated InventoryItem to handle dynamic data

--- frontend-src/components/InventoryView.js
+++ frontend-src/components/InventoryView.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { ScrollView, StyleSheet, View, Dimensions } from 'react-native';
 import { Card, Title, Text } from 'react-native-paper';
 
+// TODO: BBQ-25 Replace this stub with real data
+const inventoryDayItems = [
+    {
+        item_name: 'Beef Brisket',
+        item_quantity: 13.2
+    },
+    {
+        item_name: 'Ribs',
+        item_quantity: 9.0
+    }
+];
+
 export class InventoryView extends React.Component {
     render() {
         return (
             <ScrollView contentContainerStyle={styles.inventoryView}>
                 <View style={styles.inventoryRow}>
-                    <InventoryItem />
+                    <InventoryItem initialItemName={inventoryDayItems[0].item_name} initialItemQuantity={inventoryDayItems[0].item_quantity} />
                     <InventoryItem />
                     <InventoryItem />
                 </View>
@@ -22,17 +34,43 @@ export class InventoryView extends React.Component {
 }
 
 class InventoryItem extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            itemName: this.props.initialItemName,
+            itemQuantity: this.props.initialItemQuantity
+        }
+
+    }
+
+    updateItemName(newItemName) {
+        this.setState({
+            itemName: newItemName
+        });
+    }
+
+    updateItemQuantity(newItemQuantity) {
+        this.setState({
+            itemQuantity: newItemQuantity
+        });
+    }
+
     render() {
         return (
             <Card style={styles.inventoryItemCard}>
                 <View style={styles.inventoryTextBlock}>
-                    <Text style={{textAlign: 'center'}}>13.0</Text>
-                    <Text>Brisket</Text>
+                    <Text style={styles.inventoryItemQuantity}>{this.state.itemQuantity}</Text>
+                    <Text style={styles.inventoryItemName}>{this.state.itemName}</Text>
                 </View>        
             </Card>
         );
         
     }
+}
+
+InventoryItem.defaultProps = {
+    initialItemName: 'Item Name',
+    initialItemQuantity: '##.#'
 }
 
 const stylesSettings = {
@@ -54,6 +92,14 @@ const stylesSettings = {
         flex: 1, 
         flexDirection: 'column', 
         justifyContent: 'center',
+    },
+    inventoryItemQuantity: {
+        textAlign: 'center',
+        fontSize: 48,
+    },
+    inventoryItemName: {
+        textAlign: 'center',
+        fontSize: 20,
     },
 };
 
